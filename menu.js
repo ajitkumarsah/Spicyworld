3c055517b6fcaf0a774a07996a9cf6ccab461458
unknown <ajitkumarsah7722@gmail.com>
2022-09-28 22:35:27 +0530

    added

---
+++ menu.js
@@ -0,0 +1,225 @@
+
+        const showButtons = () => {
+            document.getElementById("btn1").style.display = "block"
+            document.getElementById("btn2").style.display = "block"
+            document.getElementById("btn3").style.display = "block"
+            document.getElementById("btn4").style.display = "block"
+            document.getElementById("btn5").style.display = "block"
+            document.getElementById("btn6").style.display = "block"
+            document.getElementById("btn7").style.display = "block"
+            document.getElementById("btn8").style.display = "block"
+        }
+        const showi0 = () => {
+            document.getElementById("image8").style.display = "block"
+            document.getElementById("image9").style.display = "none"
+            document.getElementById("image10").style.display = "none"
+            document.getElementById("image11").style.display = "none"
+            document.getElementById("image12").style.display = "none"
+            document.getElementById("image13").style.display = "none"
+            document.getElementById("image14").style.display = "none"
+            document.getElementById("image15").style.display = "none"
+        }
+        const showi1 = () => {
+            document.getElementById("image8").style.display = "none"
+            document.getElementById("image9").style.display = "block"
+            document.getElementById("image10").style.display = "none"
+            document.getElementById("image11").style.display = "none"
+            document.getElementById("image12").style.display = "none"
+            document.getElementById("image13").style.display = "none"
+            document.getElementById("image14").style.display = "none"
+            document.getElementById("image15").style.display = "none"
+        }
+        const showi2 = () => {
+            document.getElementById("image8").style.display = "none"
+            document.getElementById("image9").style.display = "none"
+            document.getElementById("image10").style.display = "block"
+            document.getElementById("image11").style.display = "none"
+            document.getElementById("image12").style.display = "none"
+            document.getElementById("image13").style.display = "none"
+            document.getElementById("image14").style.display = "none"
+            document.getElementById("image15").style.display = "none"
+        }
+        const showi3 = () => {
+            document.getElementById("image8").style.display = "none"
+            document.getElementById("image9").style.display = "none"
+            document.getElementById("image10").style.display = "none"
+            document.getElementById("image11").style.display = "block"
+            document.getElementById("image12").style.display = "none"
+            document.getElementById("image13").style.display = "none"
+            document.getElementById("image14").style.display = "none"
+            document.getElementById("image15").style.display = "none"
+        }
+        const showi4 = () => {
+            document.getElementById("image8").style.display = "none"
+            document.getElementById("image9").style.display = "none"
+            document.getElementById("image10").style.display = "none"
+            document.getElementById("image11").style.display = "none"
+            document.getElementById("image12").style.display = "block"
+            document.getElementById("image13").style.display = "none"
+            document.getElementById("image14").style.display = "none"
+            document.getElementById("image15").style.display = "none"
+        }
+        const showi5 = () => {
+            document.getElementById("image8").style.display = "none"
+            document.getElementById("image9").style.display = "none"
+            document.getElementById("image10").style.display = "none"
+            document.getElementById("image11").style.display = "none"
+            document.getElementById("image12").style.display = "none"
+            document.getElementById("image13").style.display = "block"
+            document.getElementById("image14").style.display = "none"
+            document.getElementById("image15").style.display = "none"
+        }
+        const showi6 = () => {
+            document.getElementById("image8").style.display = "none"
+            document.getElementById("image9").style.display = "none"
+            document.getElementById("image10").style.display = "none"
+            document.getElementById("image11").style.display = "none"
+            document.getElementById("image12").style.display = "none"
+            document.getElementById("image13").style.display = "none"
+            document.getElementById("image14").style.display = "block"
+            document.getElementById("image15").style.display = "none"
+        }
+        const showi7 = () => {
+            document.getElementById("image8").style.display = "none"
+            document.getElementById("image9").style.display = "none"
+            document.getElementById("image10").style.display = "none"
+            document.getElementById("image11").style.display = "none"
+            document.getElementById("image12").style.display = "none"
+            document.getElementById("image13").style.display = "none"
+            document.getElementById("image14").style.display = "none"
+            document.getElementById("image15").style.display = "block"
+        }
+
+        // this part for meal 
+
+        
+        const showButtons1 = () => {
+            document.getElementById("btn9").style.display = "block"
+            document.getElementById("btn10").style.display = "block"
+            document.getElementById("btn11").style.display = "block"
+            document.getElementById("btn12").style.display = "block"
+            document.getElementById("btn13").style.display = "block"
+            document.getElementById("btn14").style.display = "block"
+            document.getElementById("btn15").style.display = "block"
+            document.getElementById("btn16").style.display = "block"
+        }
+
+        const show = () => {
+            document.getElementById("image").style.display = "block"
+            document.getElementById("image1").style.display = "none"
+            document.getElementById("image2").style.display = "none"
+            document.getElementById("image3").style.display = "none" 
+            document.getElementById("image4").style.display = "none"
+            document.getElementById("image5").style.display = "none"  
+            document.getElementById("image6").style.display = "none" 
+            document.getElementById("image7").style.display = "none" 
+        }
+        const show1 = () => {
+            document.getElementById("image1").style.display = "block"
+            document.getElementById("image").style.display = "none"
+            document.getElementById("image2").style.display = "none"
+            document.getElementById("image3").style.display = "none" 
+            document.getElementById("image4").style.display = "none" 
+            document.getElementById("image5").style.display = "none"
+            document.getElementById("image6").style.display = "none"  
+            document.getElementById("image7").style.display = "none" 
+        }
+        const show2 = () => {
+            document.getElementById("image1").style.display = "none"
+            document.getElementById("image").style.display = "none"
+            document.getElementById("image3").style.display = "none" 
+            document.getElementById("image2").style.display = "block"
+            document.getElementById("image4").style.display = "none" 
+            document.getElementById("image5").style.display = "none" 
+            document.getElementById("image6").style.display = "none" 
+            document.getElementById("image7").style.display = "none" 
+        }
+        const show3 = () => {
+            document.getElementById("image1").style.display = "none"
+            document.getElementById("image").style.display = "none"
+            document.getElementById("image2").style.display = "none"
+            document.getElementById("image4").style.display = "none" 
+            document.getElementById("image3").style.display = "block" 
+            document.getElementById("image5").style.display = "none"  
+            document.getElementById("image6").style.display = "none" 
+            document.getElementById("image7").style.display = "none"           
+            
+        }
+        const show4 = () => {
+            document.getElementById("image1").style.display = "none"
+            document.getElementById("image").style.display = "none"
+            document.getElementById("image2").style.display = "none"
+            document.getElementById("image3").style.display = "none" 
+            document.getElementById("image4").style.display = "block"  
+            document.getElementById("image5").style.display = "none" 
+            document.getElementById("image6").style.display = "none" 
+            document.getElementById("image7").style.display = "none"           
+            
+        }
+        const show5 = () => {
+            document.getElementById("image1").style.display = "none"
+            document.getElementById("image").style.display = "none"
+            document.getElementById("image2").style.display = "none"
+            document.getElementById("image3").style.display = "none" 
+            document.getElementById("image4").style.display = "none"  
+            document.getElementById("image5").style.display = "block" 
+            document.getElementById("image6").style.display = "none" 
+            document.getElementById("image7").style.display = "none"           
+            
+        }
+        const show6 = () => {
+            document.getElementById("image1").style.display = "none"
+            document.getElementById("image").style.display = "none"
+            document.getElementById("image2").style.display = "none"
+            document.getElementById("image3").style.display = "none" 
+            document.getElementById("image4").style.display = "none"  
+            document.getElementById("image5").style.display = "none" 
+            document.getElementById("image6").style.display = "block" 
+            document.getElementById("image7").style.display = "none"           
+            
+        }
+        const show7 = () => {
+            document.getElementById("image1").style.display = "none"
+            document.getElementById("image").style.display = "none"
+            document.getElementById("image2").style.display = "none"
+            document.getElementById("image3").style.display = "none" 
+            document.getElementById("image4").style.display = "none"  
+            document.getElementById("image5").style.display = "none" 
+            document.getElementById("image6").style.display = "none" 
+            document.getElementById("image7").style.display = "block"           
+            
+        }
+
+        // this part for sweet 
+
+        const showButtons2= () => {
+            document.getElementById("btn17").style.display = "block"
+            document.getElementById("btn18").style.display = "block"
+            document.getElementById("btn19").style.display = "block"
+            document.getElementById("btn20").style.display = "block"
+        }
+
+        const showo0 = () => {
+            document.getElementById("image16").style.display = "block"
+            document.getElementById("image17").style.display = "none"
+            document.getElementById("image18").style.display = "none"
+            document.getElementById("image19").style.display = "none"  
+        }
+        const showo1 = () => {
+            document.getElementById("image16").style.display = "none"
+            document.getElementById("image17").style.display = "block"
+            document.getElementById("image18").style.display = "none"
+            document.getElementById("image19").style.display = "none"  
+        }
+        const showo2 = () => {
+            document.getElementById("image16").style.display = "none"
+            document.getElementById("image17").style.display = "none"
+            document.getElementById("image18").style.display = "block"
+            document.getElementById("image19").style.display = "none"  
+        }
+        const showo3 = () => {
+            document.getElementById("image16").style.display = "none"
+            document.getElementById("image17").style.display = "none"
+            document.getElementById("image18").style.display = "none"
+            document.getElementById("image19").style.display = "block"  
+        }
